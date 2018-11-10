62b1eb61a70b68e2df27d7add5a19070c85a92b6
alexengrig <alexengrig@gmail.com>
2018-11-10 23:57:17 +0300

    Create controller of message

---
+++ src/main/java/alexengrig/giggler/controller/APIURLs.java
@@ -0,0 +1,9 @@
+package alexengrig.giggler.controller;
+
+public interface APIURLs {
+
+    String API_URL = "/api";
+
+    String API_MESSAGE_URL = API_URL + "/message";
+
+}
---
+++ src/main/java/alexengrig/giggler/controller/MessageController.java
@@ -0,0 +1,56 @@
+package alexengrig.giggler.controller;
+
+import alexengrig.giggler.domain.Message;
+import alexengrig.giggler.repository.MessageRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static alexengrig.giggler.controller.APIURLs.API_MESSAGE_URL;
+
+@RestController
+@RequestMapping(API_MESSAGE_URL)
+public class MessageController {
+
+    private final MessageRepository messageRepository;
+
+
+    @Autowired
+    public MessageController(MessageRepository messageRepository) {
+        this.messageRepository = messageRepository;
+    }
+
+
+    @PostMapping
+    public ResponseEntity<?> createMessage(@RequestBody Message message) {
+        Message savedMessage = messageRepository.save(message);
+        return ResponseEntity.ok(savedMessage);
+    }
+
+    @GetMapping("{messageId}")
+    public ResponseEntity<?> getMessage(@PathVariable("messageId") Message message) {
+        return ResponseEntity.ok(message);
+    }
+
+    @GetMapping("{messageId}")
+    public ResponseEntity<?> updateMessage(@PathVariable("messageId") Message messageFromDb,
+                                           @RequestBody Message message) {
+        BeanUtils.copyProperties(message, messageFromDb);
+        Message updatedMessage = messageRepository.save(messageFromDb);
+        return ResponseEntity.ok(updatedMessage);
+    }
+
+    @DeleteMapping("{messageId}")
+    public ResponseEntity<?> deleteMessage(@PathVariable("messageId") Message message) {
+        messageRepository.delete(message);
+        return ResponseEntity.ok().build();
+    }
+
+
+    @GetMapping
+    public ResponseEntity<?> getMessages() {
+        return ResponseEntity.ok(messageRepository.findAll());
+    }
+
+}
