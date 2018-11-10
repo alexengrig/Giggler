5fb2f20f167393d8b3fc3d64313f06fa5247f740
alexengrig <alexengrig@gmail.com>
2018-11-10 22:53:57 +0300

    Initial project

---
+++ src/main/java/alexengrig/giggler/GigglerApplication.java
@@ -0,0 +1,12 @@
+package alexengrig.giggler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigglerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigglerApplication.class, args);
+	}
+}
